926aa64c9935c73c4ad579f3800a3f611b03d283
DiamondSL <diamondsl.dg9@gmail.com>
2022-03-28 15:32:03 +0300

    Updates

---
+++ assets/index.71c54382.js
@@ -0,0 +1,2 @@
+var a=Object.defineProperty;var o=(e,t)=>a(e,"name",{value:t,configurable:!0});import{j as r,q as n}from"./vendor.c7af32f3.js";var s=Object.defineProperty,u=o((e,t)=>s(e,"name",{value:t,configurable:!0}),"__name");const p=u(()=>r(n,{status:"404",title:"Requested page not found"}),"NotFound");export{p as default};
+//# sourceMappingURL=index.71c54382.js.map
